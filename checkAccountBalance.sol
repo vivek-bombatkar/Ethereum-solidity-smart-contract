6e94840ee7bf010fb662aa2982d8a0a478704d85
Vivek <bombatkar.vivek@gmail.com>
2018-03-10 20:43:12 +0100

    Create checkAccountBalance.sol

---
+++ checkAccountBalance.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.0;
+
+/// @title account balance 
+
+contract Payable{
+    
+    function Payable(){
+        
+    }
+    
+    function rceiveFunda() payable{
+        
+    }
+    function getBalance() constant returns(uint){
+        return this.balance;
+    }
+}
